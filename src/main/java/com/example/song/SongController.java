74677f44c9cc0dc20f9a6b31847a9f9f2b96c677
sravanthibangaru <saisravanthibangaru@gmail.com>
2023-03-13 03:07:52 +0000

    song application solution

---
+++ src/main/java/com/example/song/SongController.java
@@ -0,0 +1,39 @@
+package com.example.song;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import com.example.song.Song;
+import com.example.song.SongService;
+ 
+// Write your code here
+@RestController
+public class SongController{
+
+    SongService songService = new SongService();
+
+    @GetMapping("/songs")
+    public ArrayList<Song> getAllSongs(){
+        return songService.getAllSongs();
+    }
+
+    @GetMapping("/songs/{songId}")
+    public Song getSongById(@PathVariable("songId") int songId){
+        return songService.getSongById(songId);
+    }
+
+    @PostMapping("/songs")
+    public Song addSong(@RequestBody Song song){
+        return songService.addSong(song);
+    }
+
+    @PutMapping("/songs/{songId}")
+    public Song updateSong(@PathVariable("songId")int songId, @RequestBody Song song){
+        return songService.updateSong(songId,song);
+    }
+
+    @DeleteMapping("/songs/{songId}")
+    public void deleteSong(@PathVariable("songId") int songId){
+        songService.deleteSong(songId);
+    }
+
+}
